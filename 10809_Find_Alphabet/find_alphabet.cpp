b60119060c90d08594cf5ec0650b9593a68f3ec2
rainBuildingTree <gpark0303@outlook.com>
2022-07-29 16:14:54 +0900

    find_alphabet v1.0.

---
+++ 10809_Find_Alphabet/find_alphabet.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+/*
+MAIN:
+    GET sentence input
+
+    CREATE integer array with 26 length
+    SET all members in array to be 0
+
+    FOR: scan for whole sentence
+        SET array[recently scanned alphabet - 'a'] += 1
+    
+    FOR scan the array.
+        PRINT member of array
+        IF current member is not a last member in the array
+            PRINT space
+*/
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string sentence;
+    cin >> sentence;
+
+    int* array = new int[26];
+    for (int i = 0; i < 26; ++i)
+        array[i] = -1;
+
+    const int sentence_size = sentence.length();
+    for (int i = 0; i < sentence_size; ++i) {
+        if (array[sentence[i] - 'a'] == -1)
+            array[sentence[i] - 'a'] = i;
+    }
+
+    for (int i = 0; i < 26; ++i) {
+        cout << array[i];
+        if (i != 25)
+            cout << ' ';
+    }
+    
+    return 0;
+}
