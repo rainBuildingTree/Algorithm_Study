a1113df1dbb6ce826f4c950f0dd551f3bce132ce
rainBuildingTree <gpark0303@outlook.com>
2022-08-01 16:18:20 +0900

    mod-operation v1.0.

---
+++ 10430_modulo_operation/modulo_opeartion.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+/*
+문제
+(A+B)%C는 ((A%C) + (B%C))%C 와 같을까?
+
+(A×B)%C는 ((A%C) × (B%C))%C 와 같을까?
+
+세 수 A, B, C가 주어졌을 때, 위의 네 가지 값을 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 A, B, C가 순서대로 주어진다. (2 ≤ A, B, C ≤ 10000)
+
+출력
+첫째 줄에 (A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C, 셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C를 출력한다.
+*/
+
+int main() {
+    unsigned int A, B, C;
+    cin >> A >> B >> C;
+    cout << (A+B)%C << '\n' << ((A%C)+(B%C))%C << '\n' << (A*B)%C << '\n' << ((A%C)*(B%C))%C;
+    return 0;
+}
