a30b6a519b0eb87b7a95cba952b29328d181f840
rbtree <rbtree0303@gmail.com>
2023-04-04 20:04:03 +0800

    refactor(main): clean the code(#2)
    - 'menPref' now does not work for current preference index
    - 'prefIndex' works for current preference index
    - functions for checking unpaired men deleted
    - a stack is used to check unpaired men
    - divided areas for each function
    - deleted (something - 1) to clarify the intention
    
    Closes: #2

--- Solved Archive/12022_Pair/pair_stm.cpp
+++ Solved Archive/12022_Pair/pair_stm.cpp
@@ -1,52 +1,37 @@
 #include <iostream>
+#include <stack>
 using namespace std;
-
-bool IsAllPaired(int* wife, int peopleCount)
-{
-    bool isAllPaired = true;
-
-    for (int i = 0; i < peopleCount; ++i)
-        if (wife[i] < 0)
-            isAllPaired = false;
-
-    return isAllPaired; 
-}
-
-int FindUnpairedMan(int* wife, int peopleCount)
-{
-    for (int i = 0; i < peopleCount; ++i)
-        if (wife[i] < 0)
-            return i;
-    
-    return -1;
-}
 
 int main()
 {
+    //==================================================
+    /* CONFIG */
+
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    // Get number of men/women
+    /* CONFIG END */
+    //==================================================
+
+
+
+    //==================================================
+    /* INPUT */
+
     int peopleCount;
     cin >> peopleCount;
     
-    // Make a men preference table
     int** menPref = new int*[peopleCount];
     for (int i = 0; i < peopleCount; ++i)
-        menPref[i] = new int[peopleCount+1];
-    
-    for (int i = 0; i < peopleCount; ++i)
-    {
-        menPref[i][0] = 1;
-        for (int j = 1; j <= peopleCount; ++j)
-            cin >> menPref[i][j];
-    }
+        menPref[i] = new int[peopleCount];
 
-    // Make a inversed women preference table
+    for (int i = 0; i < peopleCount; ++i)
+        for (int j = 0; j < peopleCount; ++j)
+            cin >> menPref[i][j];
+
     int** inverseWomenPref = new int*[peopleCount];
     for (int i = 0; i < peopleCount; ++i)
         inverseWomenPref[i] = new int[peopleCount];
-    
     for (int i = 0; i < peopleCount; ++i)
         for (int j = 0; j < peopleCount; ++j)
         {
@@ -55,55 +40,93 @@ int main()
             inverseWomenPref[i][temp-1] = j;
         }
 
-    // Make wife / husband arrays
+    /* INPUT END */
+    //==================================================
+
+
+
+    //==================================================
+    /* DATA */
+
+    int* prefIndex = new int[peopleCount];
+    for (int i = 0; i < peopleCount; ++i)
+        prefIndex[i]= 0;
+
     int* wife = new int[peopleCount];
     int* husband = new int[peopleCount];
-
-    // Initialize with -1
     for (int i = 0; i < peopleCount; ++i)
     {
-        wife[i] = -1;
-        husband[i] = -1;
+        wife[i] = 0;
+        husband[i] = 0;
     }
 
-    // Stable Marriage Algorithm
-    while (!IsAllPaired(wife, peopleCount))
+    stack<int> unpairedMen;
+    for (int i = 1; i <= peopleCount; ++i)
+        unpairedMen.push(i);
+
+    /* DATA END */
+    //==================================================
+
+
+
+    //==================================================
+    /* PROCESS */
+
+    while (!unpairedMen.empty())
     {
-        int man = FindUnpairedMan(wife, peopleCount);
+        int man = unpairedMen.top();
+        int manIndex = man - 1;
+        unpairedMen.pop();
         
-        while (wife[man] < 0)
+        while (wife[manIndex] == 0)
         {
-            if (menPref[man][0] > peopleCount)
-            {
-                cout << "\n\nERROR: perfection broken\n\n";
-                break;
-            }
+            int woman = menPref[manIndex][prefIndex[manIndex]];
+            int womanIndex = woman - 1;
+            ++prefIndex[manIndex];
 
-            int woman = menPref[man][menPref[man][0]] - 1;
-            ++menPref[man][0];
-
-            if (husband[woman] < 0)
+            if (husband[womanIndex] == 0)
             {
-                wife[man] = woman;
-                husband[woman] = man;
+                wife[manIndex] = woman;
+                husband[womanIndex] = man;
             }
-            else if (inverseWomenPref[woman][man] < inverseWomenPref[woman][husband[woman]])
+            else
             {
-                wife[man] = woman;
-                wife[husband[woman]] = -1;
-                husband[woman] = man;
+                int victim = husband[womanIndex];
+                int victimIndex = victim - 1;
+
+                if (inverseWomenPref[womanIndex][manIndex] < inverseWomenPref[womanIndex][victimIndex])
+                {
+                    wife[victimIndex] = 0;
+                    unpairedMen.push(victim);
+
+                    wife[manIndex] = woman;
+                    husband[womanIndex] = man;
+                }
             }
         }
     }
 
+    /* PROCESS END */
+    //==================================================
+
+
+
+    //==================================================
+    /* OUTPUT */
+
     for (int i = 0; i < peopleCount; ++i)
-        cout << wife[i]+1 << '\n';
+        cout << wife[i] << '\n';
+
+    /* OUTPUT END*/
+    //==================================================
 
 
-    // Deallocation of memory
+
+    //==================================================
+    /* MEMORY DEALLOCATION */
+
     delete [] wife;
     delete [] husband;
-
     for (int i = 0; i < peopleCount; ++i)
     {
         delete [] menPref[i];
@@ -111,9 +134,14 @@ int main()
     }
     delete [] menPref;
     delete [] inverseWomenPref;
+    delete [] prefIndex;
+
+    /* MEMORY DEALLOCATION END */
+    //==================================================
 
 
 
-
+    //==================================================
+    /* RETURN */
     return 0;
 }
