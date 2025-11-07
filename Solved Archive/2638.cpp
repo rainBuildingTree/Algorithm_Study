fbe77f4af59f15d35fb434c7145f760ecb4ca7a7
rbtree <rbtree0303@gmail.com>
2025-11-07 11:57:14 +0900

    Add 2638 solution

--- Solved Archive/2638.cpp
+++ Solved Archive/2638.cpp
@@ -2,83 +2,81 @@
 #include <vector>
 #include <queue>
 #include <utility>
-#include <cmath>
-#include <array>
 using namespace std;
-
-struct Cheese {
-	int melted_at = -1;
-	int air_contact = 0;
-};
 
 int main() {
 	int N, M;
 	cin >> N >> M;
 
-	vector<vector<Cheese>> paper(N, vector<Cheese>(M, Cheese()));
+	int cheese_count = 0;
+	vector<vector<int>> cheeses(N, vector<int>(M, 0));
+	vector<vector<int>> map(N, vector<int>(M, 0));
 	for (int i = 0; i < N; ++i)
 		for (int j = 0; j < M; ++j) {
-			int input;
-			cin >> input;
-			if (input == 0) paper[i][j].melted_at = 0;
+			cin >> cheeses[i][j];
+			map[i][j] = cheeses[i][j];
+			if (cheeses[i][j]) ++cheese_count;
 		}
-	
-	queue<array<int, 3>> bfs_queue;
-	int max_melted_at = 0;
-	for (int i = 0; i < N; ++i) {
-		for (int j = 0; j < M; ++j) {
-			if (i > 0 && paper[i-1][j].melted_at == 0)
-				++paper[i][j].air_contact;
-			if (i < N-1 && paper[i+1][j].melted_at == 0)
-				++paper[i][j].air_contact;
-			if (j > 0 && paper[i][j-1].melted_at == 0)
-				++paper[i][j].air_contact;
-			if (j < M-1 && paper[i][j+1].melted_at == 0)
-				++paper[i][j].air_contact;
-			
-			if (paper[i][j].melted_at < 0 && paper[i][j].air_contact > 1) {
-				bfs_queue.push({{i, j, 1}});
+
+	int time = 0;
+	while (cheese_count) {
+		vector<vector<int>> visited(N, vector<int>(M, 0));
+		queue<pair<int, int>> bfs_queue;
+		bfs_queue.push({ 0,0 });
+		map[0][0] = 2;
+
+		while (!bfs_queue.empty()) {
+			int r = bfs_queue.front().first;
+			int c = bfs_queue.front().second;
+			bfs_queue.pop();
+
+			if (r > 0 && !visited[r - 1][c] && !cheeses[r - 1][c]) {
+				map[r - 1][c] = 2;
+				visited[r - 1][c] = 1;
+				bfs_queue.push({ r - 1,c });
+			}
+			if (r < N - 1 && !visited[r + 1][c] && !cheeses[r + 1][c]) {
+				map[r + 1][c] = 2;
+				visited[r + 1][c] = 1;
+				bfs_queue.push({ r + 1,c });
+			}
+			if (c > 0 && !visited[r][c - 1] && !cheeses[r][c - 1]) {
+				map[r][c - 1] = 2;
+				visited[r][c - 1] = 1;
+				bfs_queue.push({ r,c - 1 });
+			}
+			if (c < M - 1 && !visited[r][c + 1] && !cheeses[r][c + 1]) {
+				map[r][c + 1] = 2;
+				visited[r][c + 1] = 1;
+				bfs_queue.push({ r,c + 1 });
 			}
 		}
+
+		for (int r = 0; r < N; ++r) {
+			for (int c = 0; c < M; ++c) {
+				if (!cheeses[r][c])
+					continue;
+				int air_count = 0;
+				if (r > 0 && map[r - 1][c] == 2)
+					++air_count;
+				if (r < N - 1 && map[r + 1][c] == 2)
+					++air_count;
+				if (c > 0 && map[r][c - 1] == 2)
+					++air_count;
+				if (c < M - 1 && map[r][c + 1] == 2)
+					++air_count;
+
+				if (air_count > 1) {
+					cheeses[r][c] = 0;
+					--cheese_count;
+				}
+			}
+		}
+
+		++time;
 	}
 
-	while (!bfs_queue.empty()) {
-		int x = bfs_queue.front()[0];
-		int y = bfs_queue.front()[1];
-		int time = bfs_queue.front()[2];
-		bfs_queue.pop();
-
-		if (paper[x][y].melted_at >= 0)
-			continue;
-		if (paper[x][y].air_contact > 1)
-			paper[x][y].melted_at = time;
-		if (paper[x][y].melted_at < 0)
-			continue;
-		max_melted_at = max(max_melted_at, time);
-
-		if (x > 0 && paper[x-1][y].melted_at < 0) {
-			++paper[x-1][y].air_contact;
-			if (paper[x-1][y].air_contact > 1)
-				bfs_queue.push({{x-1, y, time+1}});
-		}
-		if (x < N-1 && paper[x+1][y].melted_at < 0) {
-			++paper[x+1][y].air_contact;
-			if (paper[x+1][y].air_contact > 1)
-				bfs_queue.push({{x+1, y, time+1}});
-		}
-		if (y > 0 && paper[x][y-1].melted_at < 0) {
-			++paper[x][y-1].air_contact;
-			if (paper[x][y-1].air_contact > 1)
-				bfs_queue.push({{x, y-1, time+1}});
-		}
-		if (y > 0 && paper[x][y+1].melted_at < 0) {
-			++paper[x][y+1].air_contact;
-			if (paper[x][y+1].air_contact > 1)
-				bfs_queue.push({{x, y+1, time+1}});
-		}
-	}
-
-	cout << max_melted_at;
+	cout << time;
 
 	return 0;
 }
